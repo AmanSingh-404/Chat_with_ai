c252c3429b39c4fa40a1a1e82b287c034a609999
Aman Singh <amanscode2005@gmail.com>
2026-02-19 20:17:08 +0530

    add create chat backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const cookieparser = require("cookie-parser")
 const AuthRoutes = require("./routes/auth.routes")
+const chatRoutes = require("../src/routes/chat.routes")
 
 const app = express()
 app.use(express.json())
 app.use(cookieparser())
 app.use('/api/auth', AuthRoutes)
+app.use('/api/chat', chatRoutes)
 
 module.exports = app
